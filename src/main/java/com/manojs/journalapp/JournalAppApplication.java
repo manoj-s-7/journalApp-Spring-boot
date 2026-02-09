4fc5daecc2a3c76b92d10b6687fba8254dca3fc5
Manoj <manojcs6317@gmail.com>
2026-02-09 17:01:55 +0530

    first commit

---
+++ src/main/java/com/manojs/journalapp/JournalAppApplication.java
@@ -0,0 +1,13 @@
+package com.manojs.journalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalAppApplication.class, args);
+	}
+
+}
